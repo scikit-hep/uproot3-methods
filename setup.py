448128421c97ed4f88da7e899571a2b135a9ff91
Jim Pivarski <jpivarski@gmail.com>
2018-12-13 14:49:47 -0600

    require version 0.7.0 or later of awkward

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(name = "uproot-methods",
       download_url = "https://github.com/scikit-hep/uproot-methods/releases",
       license = "BSD 3-clause",
       test_suite = "tests",
-      install_requires = ["numpy>=1.13.1", "awkward"],
+      install_requires = ["numpy>=1.13.1", "awkward>=0.7.0"],
       tests_require = [],
       classifiers = [
           "Development Status :: 4 - Beta",
