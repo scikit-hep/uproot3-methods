945785c82077aa2657a0217c6092de04bdc60672
Jim Pivarski <jpivarski@gmail.com>
2019-04-11 13:09:43 -0500

    [skip ci] Get the URL right

--- uproot_methods/__init__.py
+++ uproot_methods/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# BSD 3-Clause License; see https://github.com/scikit-hep/uproot/blob/master/LICENSE
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot-methods/blob/master/LICENSE
 
 from uproot_methods.classes.TVector2 import TVector2, TVector2Array
 from uproot_methods.classes.TVector3 import TVector3, TVector3Array
