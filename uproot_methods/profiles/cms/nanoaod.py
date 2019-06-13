4faf43ab87b3b46577c47ce9b49f230151bfa705
Jim Pivarski <jpivarski@gmail.com>
2019-06-13 10:29:11 -0500

    can translate muons in tree.lazyarrays

--- uproot_methods/profiles/cms/nanoaod.py
+++ uproot_methods/profiles/cms/nanoaod.py
@@ -2,106 +2,113 @@
 
 # BSD 3-Clause License; see https://github.com/scikit-hep/uproot-methods/blob/master/LICENSE
 
-import tokenize
-import keyword
-import re
-from collections import OrderedDict
+import awkward.type
 
-import awkward.array.chunked
-import awkward.array.objects
-import awkward.array.jagged
-import awkward.array.table
-import awkward.array.virtual
+# def unwrap(array, flatten):
+#     if isinstance(array, awkward.array.chunked.ChunkedArray):
+#         return array.copy(chunks=[unwrap(chunk, flatten) for chunk in array.chunks])
+#     elif isinstance(array, awkward.array.virtual.VirtualArray):
+#         return unwrap(array.array, flatten)
+#     elif isinstance(array, awkward.array.jagged.JaggedArray):
+#         if flatten:
+#             return array.content
+#         else:
+#             return array
+#     else:
+#         raise NotImplementedError(type(array))
 
-def _isidentifier(x):
-    return isinstance(x, str) and _isidentifier.regex.match(x) and not keyword.iskeyword(x)
-_isidentifier.regex = re.compile("^" + tokenize.Name + "$")
+# class GenerateJaggedTable(object):
+#     def __init__(self, JaggedArray, Table, rowname, counts):
+#         self.JaggedArray = JaggedArray
+#         self.Table = Table
+#         self.rowname = rowname
+#         self.counts = counts
+#         self.fields = OrderedDict()
 
-class Attribute(awkward.array.objects.Methods):
-    def __setitem__(self, where, what):
-        super(Attribute, self).__setitem__(where, what)
-        if _isidentifier(where) and not where in dir(awkward.array.table.Table) and not where in dir(awkward.array.jagged.JaggedArray):
-            self.__dict__[where] = self._contents[where]
+#     def __call__(self):
+#         # table = self.Table.named(self.rowname)
+#         # for n, x in self.fields.items():
+#         #     table[n] = awkward.array.virtual.VirtualArray(unwrap, x)
 
-    def __delitem__(self, where):
-        if _isidentifier(where) and where in self._contents and where in self.__dict__:
-            del self.__dict__[where]
-        super(Attribute, self).__delitem__(where)
+#         VirtualArray = self.counts.VirtualArray
+#         ChunkedArray = self.counts.ChunkedArray
+#         return ChunkedArray([VirtualArray(makejagged, chunk) for chunk in self.counts.chunks], self.counts.counts)
 
-def unwrap(array, flatten):
-    if isinstance(array, awkward.array.chunked.ChunkedArray):
-        return array.copy(chunks=[unwrap(chunk, flatten) for chunk in array.chunks])
-    elif isinstance(array, awkward.array.virtual.VirtualArray):
-        return unwrap(array.array, flatten)
-    elif isinstance(array, awkward.array.jagged.JaggedArray):
-        if flatten:
-            return array.content
-        else:
-            return array
-    else:
-        raise NotImplementedError(type(array))
+#     # def __call__(self, rowgroup, column):
+#     #     return fromarrow(self.parquetfile.read_row_group(rowgroup, columns=[column]))[column]
+
+#     # def __getstate__(self):
+#     #     return {"file": self.file, "metadata": self.metadata, "common_metadata": self.common_metadata}
+
+#     # def __setstate__(self, state):
+#     #     self.file = state["file"]
+#     #     self.metadata = state["metadata"]
+#     #     self.common_metadata = state["common_metadata"]
+#     #     self._init()
+
+#     # def tojson(self):
+#     #     json.dumps([self.file, self.metadata, self.common_metadata])
+#     #     return {"file": self.file, "metadata": self.metadata, "common_metadata": self.common_metadata}
+
+#     # @classmethod
+#     # def fromjson(cls, state):
+#     #     return cls(state["file"], metadata=state["metadata"], common_metadata=state["common_metadata"])
+
+def getcontent(virtual):
+    return virtual.array.content
 
 class GenerateJaggedTable(object):
-    def __init__(self, JaggedArray, Table, rowname, counts):
-        self.JaggedArray = JaggedArray
-        self.Table = Table
+    def __init__(self, rowname, counts, fields):
         self.rowname = rowname
         self.counts = counts
-        self.fields = OrderedDict()
+        self.fields = fields
 
     def __call__(self):
-        table = self.Table.named(self.rowname)
-        for n, x in self.fields.items():
-            table[n] = awkward.array.virtual.VirtualArray(unwrap, (x, True))
-        return self.JaggedArray.fromcounts(unwrap(self.counts, False), table)
-        
-    # def __call__(self, rowgroup, column):
-    #     return fromarrow(self.parquetfile.read_row_group(rowgroup, columns=[column]))[column]
+        Table = self.counts.Table
+        JaggedArray = self.counts.JaggedArray
+        VirtualArray = self.counts.VirtualArray
 
-    # def __getstate__(self):
-    #     return {"file": self.file, "metadata": self.metadata, "common_metadata": self.common_metadata}
+        offsets = JaggedArray.counts2offsets(self.counts.array)
+        table = Table.named(self.rowname)
+        for n, x in self.fields:
+            table[n] = VirtualArray(getcontent, x, type=awkward.type.ArrayType(offsets[-1], x.type.to.to), cache=self.counts.cache, persistvirtual=self.counts.persistvirtual)
+        return JaggedArray.fromoffsets(offsets, table)
 
-    # def __setstate__(self, state):
-    #     self.file = state["file"]
-    #     self.metadata = state["metadata"]
-    #     self.common_metadata = state["common_metadata"]
-    #     self._init()
-
-    # def tojson(self):
-    #     json.dumps([self.file, self.metadata, self.common_metadata])
-    #     return {"file": self.file, "metadata": self.metadata, "common_metadata": self.common_metadata}
-
-    # @classmethod
-    # def fromjson(cls, state):
-    #     return cls(state["file"], metadata=state["metadata"], common_metadata=state["common_metadata"])
-
+def lazyjagged(countsarray, rowname, fields):
+    VirtualArray = countsarray.VirtualArray
+    ChunkedArray = countsarray.ChunkedArray
+    chunks = []
+    for i, countschunk in enumerate(countsarray.chunks):
+        fieldschunks = []
+        tabletype = awkward.type.TableType()
+        for fieldname, field in fields:
+            assert field.counts[i] == countsarray.counts[i]
+            fieldschunks.append((fieldname, field.chunks[i]))
+            tabletype[fieldname] = field.type.to.to
+        generator = GenerateJaggedTable(rowname, countschunk, fieldschunks)
+        chunks.append(VirtualArray(generator, type=awkward.type.ArrayType(len(countschunk), float("inf"), tabletype), cache=countschunk.cache, persistvirtual=countschunk.persistvirtual))
+    return ChunkedArray(chunks, countsarray.counts)
 
 def transform(array):
     array._valid()
     array.check_whole_valid = False
 
-    AttributeJaggedArray = awkward.array.objects.Methods.mixin(Attribute, array.JaggedArray)
-    AttributeTable = awkward.array.objects.Methods.mixin(Attribute, array.Table)
-
-    if "nMuon" in array.columns:
-        muons = GenerateJaggedTable(AttributeJaggedArray, AttributeTable, "Muon", array["nMuon"])
-    else:
-        muons = None
-    flag = AttributeTable.named("Flags")
-    HLT = AttributeTable.named("HLT")
+    muons = []
+    flag = array.Table.named("Flags")
+    HLT = array.Table.named("HLT")
 
     for n in array.columns:
-        if n.startswith("Muon_") and muons is not None:
-            muons.fields[n[len("Muon_"):]] = array[n]
-        elif n.startswith("Flag_"):
+        if n.startswith("Muon_"):
+            muons.append((n[len("Muon_"):], array[n]))
+        if n.startswith("Flag_"):
             flag[n[len("Flag_"):]] = array[n]
         elif n.startswith("HLT_"):
             HLT[n[len("HLT_"):]] = array[n]
 
-    out = AttributeTable.named("Event")
+    out = array.Table.named("Event")
     out["raw"] = array
-    if muons is not None and len(muons.fields) > 0:
-        out["muons"] = awkward.array.virtual.VirtualArray(muons)
+    if len(muons) > 0 and "nMuon" in array.columns:
+        out["muons"] = lazyjagged(array["nMuon"], "Muon", muons)
     if len(flag.columns) > 0:
         out["flag"] = flag
     if len(HLT.columns) > 0:
