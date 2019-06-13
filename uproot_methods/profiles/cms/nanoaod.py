d4ef15dfe9869d21b97d5343c9865804fdc4cf83
Jim Pivarski <jpivarski@gmail.com>
2019-06-13 11:40:26 -0500

    now attaching p4 member

--- uproot_methods/profiles/cms/nanoaod.py
+++ uproot_methods/profiles/cms/nanoaod.py
@@ -5,6 +5,8 @@
 import awkward.type
 import awkward.array.chunked
 
+import uproot_methods.classes.TLorentzVector
+
 def getcontent(virtual):
     return virtual.array.content
 
@@ -22,6 +24,9 @@ def jaggedtable(rowname, counts, fields):
         table = Table.named(rowname)
         for n, x in fields:
             table[n] = VirtualArray(getcontent, x, type=awkward.type.ArrayType(offsets[-1], x.type.to.to), cache=counts.cache, persistvirtual=counts.persistvirtual)
+        columns = table.columns
+        if "pt" in columns and "eta" in columns and "phi" in columns and "mass" in columns and "p4" not in columns:
+            table["p4"] = VirtualArray(uproot_methods.classes.TLorentzVector.TLorentzVectorArray.from_ptetaphim, (table["pt"], table["eta"], table["phi"], table["mass"]), type=awkward.type.ArrayType(offsets[-1], uproot_methods.classes.TLorentzVector.PtEtaPhiMassLorentzVectorArray), cache=counts.cache, persistvirtual=counts.persistvirtual)
         return JaggedArray.fromoffsets(offsets, table)
 
 def lazyjagged(countsarray, rowname, fields):
@@ -35,6 +40,9 @@ def lazyjagged(countsarray, rowname, fields):
             assert field.counts[i] == countsarray.counts[i]
             fieldschunks.append((fieldname, field.chunks[i]))
             tabletype[fieldname] = field.type.to.to
+        columns = tabletype.columns
+        if "pt" in columns and "eta" in columns and "phi" in columns and "mass" in columns and "p4" not in columns:
+            tabletype["p4"] = uproot_methods.classes.TLorentzVector.TLorentzVectorArray.from_ptetaphim
         chunks.append(VirtualArray(jaggedtable, (rowname, countschunk, fieldschunks), type=awkward.type.ArrayType(len(countschunk), float("inf"), tabletype), cache=countschunk.cache, persistvirtual=countschunk.persistvirtual))
     return ChunkedArray(chunks, countsarray.counts)
 
@@ -49,10 +57,12 @@ def transform(array):
     for n in array.columns:
         if n.startswith("Muon_"):
             muons.append((n[len("Muon_"):], array[n]))
-        if n.startswith("Flag_"):
+        elif n.startswith("Flag_"):
             flag[n[len("Flag_"):]] = array[n]
         elif n.startswith("HLT_"):
             HLT[n[len("HLT_"):]] = array[n]
+        else:
+            print(n)
 
     out = array.Table.named("Event")
     out["raw"] = array
