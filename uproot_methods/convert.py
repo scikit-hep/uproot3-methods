02f9911b4437ae895a742a173eb88094e27eb65c
Jim Pivarski <jpivarski@gmail.com>
2020-11-23 12:59:10 -0600

    Rename instances of 'uproot' string to 'uproot3'.

--- uproot_methods/convert.py
+++ uproot_methods/convert.py
@@ -24,34 +24,34 @@ def towriteable(obj):
                     yield y
 
         if any(x == ("builtins", "bytes") or x == ("builtins", "str") or x == ("__builtin__", "str") or x == ("__builtin__", "unicode") for x in types(obj.__class__, obj)):
-            return (None, None, "uproot.write.objects.TObjString", "TObjString")
+            return (None, None, "uproot3.write.objects.TObjString", "TObjString")
 
         # made with numpy.histogram
         elif isinstance(obj, tuple) and len(obj) == 2 and any(x[:2] == ("numpy", "ndarray") for x in types(obj[0].__class__, obj[0])) and any(x[:2] == ("numpy", "ndarray") for x in types(obj[1].__class__, obj[1])) and len(obj[0].shape) == 1 and len(obj[1].shape) == 1 and obj[0].shape[0] == obj[1].shape[0] - 1:
-            return ("uproot_methods.classes.TH1", "from_numpy", "uproot.write.objects.TH", "TH")
+            return ("uproot_methods.classes.TH1", "from_numpy", "uproot3.write.objects.TH", "TH")
 
         # made with numpy.histogram2d
         elif isinstance(obj, tuple) and len(obj) == 3 and any(x[:2] == ("numpy", "ndarray") for x in types(obj[0].__class__, obj[0])) and any(x[:2] == ("numpy", "ndarray") for x in types(obj[1].__class__, obj[1])) and any(x[:2] == ("numpy", "ndarray") for x in types(obj[2].__class__, obj[2])) and len(obj[0].shape) == 2 and len(obj[1].shape) == 1 and len(obj[2].shape) == 1 and obj[0].shape[0] == obj[1].shape[0] - 1 and obj[0].shape[1] == obj[2].shape[0] - 1:
-            return ("uproot_methods.classes.TH2", "from_numpy", "uproot.write.objects.TH", "TH")
+            return ("uproot_methods.classes.TH2", "from_numpy", "uproot3.write.objects.TH", "TH")
 
         # made with numpy.histogramdd (2-dimensional)
         elif isinstance(obj, tuple) and len(obj) == 2 and any(x[:2] == ("numpy", "ndarray") for x in types(obj[0].__class__, obj[0])) and isinstance(obj[1], list) and len(obj[1]) == 2 and any(x[:2] == ("numpy", "ndarray") for x in types(obj[1][0].__class__, obj[1][0])) and any(x[:2] == ("numpy", "ndarray") for x in types(obj[1][1].__class__, obj[1][1])) and len(obj[0].shape) == 2 and len(obj[1][0].shape) == 1 and len(obj[1][1].shape) == 1 and obj[0].shape[0] == obj[1][0].shape[0] - 1 and obj[0].shape[1] == obj[1][1].shape[0] - 1:
-            return ("uproot_methods.classes.TH2", "from_numpy", "uproot.write.objects.TH", "TH")
+            return ("uproot_methods.classes.TH2", "from_numpy", "uproot3.write.objects.TH", "TH")
 
         elif any(x[:3] == ("pandas.core.frame", "DataFrame", "IntervalIndex") and "count" in x[3] for x in types(obj.__class__, obj)):
-            return ("uproot_methods.classes.TH1", "from_pandas", "uproot.write.objects.TH", "TH")
+            return ("uproot_methods.classes.TH1", "from_pandas", "uproot3.write.objects.TH", "TH")
 
         elif any(x == ("physt.histogram1d", "Histogram1D") for x in types(obj.__class__, obj)):
-            return ("uproot_methods.classes.TH1", "from_physt", "uproot.write.objects.TH", "TH")
+            return ("uproot_methods.classes.TH1", "from_physt", "uproot3.write.objects.TH", "TH")
 
         elif any(x == ("uproot_methods.classes.TH1", "Methods") or x == ("TH1", "Methods") for x in types(obj.__class__, obj)):
-            return (None, None, "uproot.write.objects.TH", "TH")
+            return (None, None, "uproot3.write.objects.TH", "TH")
 
         elif any(x == ("uproot_methods.classes.TH2", "Methods") or x == ("TH2", "Methods") for x in types(obj.__class__, obj)):
-            return (None, None, "uproot.write.objects.TH", "TH")
+            return (None, None, "uproot3.write.objects.TH", "TH")
 
         elif any(x == ("uproot_methods.classes.TH3", "Methods") or x == ("TH3", "Methods") for x in types(obj.__class__, obj)):
-            return (None, None, "uproot.write.objects.TH", "TH")
+            return (None, None, "uproot3.write.objects.TH", "TH")
 
         else:
             raise TypeError("type {0} from module {1} is not writeable by uproot".format(obj.__class__.__name__, obj.__class__.__module__))
